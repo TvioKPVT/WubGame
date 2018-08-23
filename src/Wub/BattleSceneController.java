4d8dcd7694a6b013ca34ece0b13e201bf2dbf92b
Tvio <new.battle.net@gmail.com>
2018-08-23 15:52:51 +0300

    asdasdasdasdasdasdasda

---
+++ src/Wub/BattleSceneController.java
@@ -0,0 +1,28 @@
+package Wub;
+
+import javafx.fxml.FXML;
+import static Wub.Main.dice;
+import static Wub.Main.enemy;
+import static Wub.Main.player;
+
+
+import java.awt.*;
+
+public class BattleSceneController {
+
+    @FXML
+    Label pstr;
+    @FXML
+    int ARGH = player.STR;
+    @FXML
+    String blyat = Integer.toString(ARGH);
+
+    @FXML
+    pstr.setText(" ");
+
+
+
+
+
+
+}
--- src/Wub/Controller.java
+++ src/Wub/Controller.java
@@ -3,7 +3,5 @@ package Wub;
 import static Wub.Main.player;
 
 public class Controller {
-    public void playerStats(){
-        pstr.setText(player.STR)
-    }
+
 }
